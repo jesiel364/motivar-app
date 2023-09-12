89945f3d4ae595a3fa48b36892f96b3834a09f33
jesiel364 <jesiel364@gmail.com>
2023-09-12 09:05:27 -0400

    feat: add grid list

--- screens/Orders/index.tsx
+++ screens/Orders/index.tsx
@@ -1,34 +1,96 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { useEffect, useState } from 'react';
+import { FlatGrid } from 'react-native-super-grid';
+import { SimpleGrid } from 'react-native-super-grid';
 
 const Orders = () => {
 
-    
-    
-    function handleScroll(){
-         
+  const ordersList = [
+    {
+      label: 'Em alta',
+      icon: '🔥'
+    },
+    {
+      label: 'Motivação',
+      icon: '💪🏼'
+    },
+    {
+      label: 'Sabedoria',
+      icon: '🧠'
+    },
+    {
+      label: 'Provérbios',
+      icon: '📖'
+    },
+    {
+      label: 'Empreendedorismo',
+      icon: '💰'
+    },
+    {
+      label: 'Reflexão',
+      icon: '🤔'
+    },
+    {
+      label: 'Otimismo',
+      icon: '🤩'
+    },
+    {
+      label: 'Disciplina',
+      icon: '🚀'
     }
+  ]
 
-    return (
-      <View style={styles.container}>
-  
- <Text>Categorias</Text>
+  function handleScroll() {
 
-        
-        <StatusBar style="auto" />
-
-      </View>
-    )
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  return (
+    <View style={styles.container}>
 
-  export default Orders
+      <FlatGrid
+        style={styles.grid}
+        itemDimension={100}
+        data={ordersList}
+        renderItem={({ item, index }) => (
+          <>
+          <Text>{item.icon}</Text>
+          <Text style={styles.gridLabel} key={index}>{item.label}</Text>
+          
+          </>
+          )}
+      />
+
+      {/* <SimpleGrid
+  itemDimension={80}
+  data={[1,2,3,4,5,6,7,8,9]}
+  renderItem={({ item }) => (<Text key={item}>{item}</Text>)}
+/> */}
+
+
+      <StatusBar style="auto" />
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+
+  grid:{
+    backgroundColor: 'red'
+  },
+
+  gridLabel:{
+    fontSize: 18
+  }
+});
+
+export default Orders

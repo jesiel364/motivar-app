65ccaf8e7c96e90fb48b8f27ad625446b6c32fac
jesieldotdev <jesiel364@gmail.com>
2023-12-20 23:52:29 -0400

    feat: splash

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@ import { ThemeProvider } from "styled-components/native";
 import Dark from "./Global/dark";
 import { PaperProvider } from "react-native-paper";
 import MyTabs from "./components/Tabs";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
 
 export type MsgProps = {
   id: number;
@@ -20,6 +22,16 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync();  
+    
+    const timerId = setTimeout(() => {
+      SplashScreen.hideAsync();  // Oculta manualmente a tela de abertura
+    }, 3000);
+
+    // Limpando o temporizador quando o componente é desmontado
+    return () => clearTimeout(timerId);// Impede que a tela de abertura seja ocultada automaticamente
+  }, []);
   return (
     <ThemeProvider theme={Dark}>
       <MyProvider>
